b233ddcba37abe0d3aff5c3e2a83d54c4019869a
Shiva Ram <sivaramon@gmail.com>
2020-02-17 20:11:39 +0530

    new changes

--- app/main.js
+++ app/main.js
@@ -8,72 +8,48 @@
         'bootstrap': '../lib/bootstrap/js/bootstrap',
         'jquery': '../lib/jquery/jquery-1.9.1',
         //chart
-        'chart': '../lib/chart/highstock/highstock',
-        'chartmore': '../lib/chart/highchart/highcharts-more',
-        'chart3d': '../lib/chart/highchart/highcharts-3d',
-        'chartexport': '../lib/chart/modules/exporting',
-        'chart-noData-display': '../lib/chart/modules/no-data-to-display',
-        'chart-heatmap': '../lib/chart/modules/heatmap',
-        'chart-treemap': '../lib/chart/modules/treemap',
-        'chart-offline-export': '../lib/chart/modules/offline-exporting',
-
-        //highcharts 
-        'highcharts': '../lib/chart/highchart/highcharts',
-        'highcharts-export': '../lib/chart/modules/exporting',
-        'highcharts-offline-exporting': '../lib/chart/modules/offline-exporting',
-        'highcharts-drilldown': '../lib/chart/modules/drilldown',
+        //'chart': '../lib/chart/highstock/highstock',
+        'highcharts': '../lib/chart/highchart/highcharts'
+        //'chartmore': 'lib/chart/highchart/highcharts-more',
+        //'chart3d': 'lib/chart/highchart/highcharts-3d',
+        //'chartexport': 'lib/chart/modules/exporting',
+        //'chart-noData-display': 'lib/chart/modules/no-data-to-display',
+        //'chart-heatmap': 'lib/chart/modules/heatmap',
+        //'chart-treemap': 'lib/chart/modules/treemap',
+        //'chart-offline-export': 'lib/chart/modules/offline-exporting'
     },
     shim: {
         'bootstrap': {
             deps: ['jquery'],
             exports: 'jQuery'
         },
-        "chart": {
-            exports: "Highcharts.Chart"
-        },
-        "chart3d": {
-            deps: ["chart"],
-            exports: "Highcharts.Chart"
-        },
-        "chartmore": {
-            deps: ["chart"],
-            exports: "Highcharts.Chart"
-        },
-        "chart-heatmap": {
-            deps: ["chart"],
-            exports: "Highcharts.Chart"
-        },
-        "chart-treemap": {
-            deps: ["chart"],
-            exports: "Highcharts.Chart"
-        },
-        "chartexport": {
-            deps: ["chart"],
-            exports: "Highcharts"
-        },
-        "chart-offline-export": {
-            deps: ["chart"],
-            exports: "Highcharts.Chart"
-        },
-        "chart-noData-display": {
-            deps: ["chart"],
-            exports: "Highcharts"
-        },
         "highcharts": {
             exports: "Highcharts"
-        },
-        "highcharts-export": {
-            deps: ["highcharts"],
-            exports: "Highcharts"
-        },
-        "highcharts-offline-exporting": {
-            deps: ["highcharts-export"],
-            exports: "Highcharts"
-        },
-        "highcharts-drilldown": {
-            deps: ["chart"],
-            exports: "Highcharts"
         }
+        //"chartmore": {
+        //    deps: ["chart"],
+        //    exports: "Highcharts.Chart"
+        //},
+        //"chart-heatmap": {
+        //    deps: ["chart"],
+        //    exports: "Highcharts.Chart"
+        //},
+        //"chart-treemap": {
+        //    deps: ["chart"],
+        //    exports: "Highcharts.Chart"
+        //},
+        //"chartexport": {
+        //    deps: ["chart"],
+        //    exports: "Highcharts"
+        //},
+        //"chart-offline-export": {
+        //    deps: ["chart"],
+        //    exports: "Highcharts.Chart"
+        //},
+        //"chart-noData-display": {
+        //    deps: ["chart"],
+        //    exports: "Highcharts"
+        //}
     }
 });
 
